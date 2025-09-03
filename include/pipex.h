bd99b9740071e842d616f8d7a15b0a7eccaaa732
JulioSouza09 <juliosouza.ux@gmail.com>
2025-09-03 11:18:33 +0100

    Still work on basic input verification

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jcesar-s <jcesar-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 17:27:33 by jcesar-s          #+#    #+#             */
-/*   Updated: 2025/09/02 16:56:47 by jcesar-s         ###   ########.fr       */
+/*   Updated: 2025/09/03 09:33:16 by jcesar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdio.h>
 # include <string.h>
 # include <errno.h>
+# include <unistd.h>
 # include "libft.h"
 
 void	test_including_file(void);
--- src/main.c
+++ src/main.c
@@ -6,26 +6,50 @@
 /*   By: jcesar-s <jcesar-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 15:42:22 by jcesar-s          #+#    #+#             */
-/*   Updated: 2025/09/02 17:54:41 by jcesar-s         ###   ########.fr       */
+/*   Updated: 2025/09/03 10:02:55 by jcesar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+void	check_access(const char *pathname, int flags)
+{
+	if (access(pathname, flags))
+	{
+		ft_printf_err("pipex: %s: %s\n", strerror(errno), pathname);
+		exit(errno);
+	}
+	return ;
+}
+
+int	open_correctly(char *pathname, int flags)
 {
 	int	fd;
+
+	fd = open(pathname, flags);
+	if (fd == -1)
+	{
+		ft_printf_err("pipex: %s: %s\n", strerror(errno), pathname);
+		exit(errno);
+	}
+	return (fd);
+}
+
+int	main(int argc, char **argv)
+{
+	int	fd_file1;
+	int	fd_file2;
+
 	if (argc != 5)
 	{
 		ft_printf_err("Usage: %s file1 cmd1 cmd2 file2\n", argv[1]);
 		return (1);
 	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		ft_printf_err("pipex: %s: %s\n", strerror(errno), argv[1]);
-		return (errno);
-	}
-	ft_printf("%d\n", fd);
+	check_access(argv[1], R_OK);
+	fd_file1 = open_correctly(argv[1], O_RDONLY);
+	check_access(argv[4], W_OK);
+	fd_file2 = open(argv[4], O_WRONLY | O_CREAT);
+	(void)fd_file2;
+	(void)fd_file1;
 	return (0);
 }
